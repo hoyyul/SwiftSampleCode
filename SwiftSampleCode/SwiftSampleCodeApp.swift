0e04462d9267c159e5ef666fd212404eee826ab7
halu <hlu07472@gmail.com>
2023-06-08 19:26:11 +0900

    Initial Commit

---
+++ SwiftSampleCode/SwiftSampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSampleCodeApp.swift
+//  SwiftSampleCode
+//
+//  Created by Lu Haoyu on 2023/06/08.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
